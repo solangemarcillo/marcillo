e7e753596c3d32190fe2c5a6fba45a072155125b
rodri <rodri@LAPTOP-SN60J7OR>
2020-01-13 12:30:16 -0500

    hola

---
+++ expo1/ApagarLucesCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace expo1
+{
+	class ApagarLucesCommand:ICommand
+	{
+		private LucesReceiver luces;
+        //pconstructtor que tiene un parametro el cual se le da un valor que 
+		public ApagarLucesCommand(LucesReceiver luces)
+		{
+			this.luces = luces;
+		}
+
+		public void Execute()
+		{
+			luces.Apagar();
+			Console.WriteLine("Apagando las luces");
+		}
+	}
+}
